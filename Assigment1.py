fc7885a41d4ad4998f0a97496b0791e71a39e0f7
Ananya Adarsh <ananya04adarshgmail.com>
2025-08-02 04:31:17 +0530

    Initial commit with assignment 1

---
+++ Assigment1.py
@@ -0,0 +1,17 @@
+
+print("Task 1")
+
+a = int(input("Enter the first Number:"))
+b = int(input("Enter the Second Number:"))
+
+print("Addition:", a + b)
+print("Substraction:", a - b)
+print("Multiplication:", a * b)
+print("Divition:", a / b)
+print()
+print("Task 2")
+print()
+first_name = input("Enter your first name:")
+last_name = input("Enter your last name:")
+
+print("Hello, ", first_name,'' ,last_name,"! Welcome to the python program.")
