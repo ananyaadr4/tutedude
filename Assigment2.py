2f8839c378f36383d553418d5f3c20a9ea35b522
ananyaadr4 <145493959+ananyaadr4@users.noreply.github.com>
2025-08-02 05:39:01 +0530

    Add files via upload
    Assigment 2

---
+++ Assigment2.py
@@ -0,0 +1,19 @@
+print("Task 1")
+print()
+a = int(input("Enter a number"))
+
+if a % 2 == 0:
+    print(a, "is a even number." )
+else:
+    print(a, "is a odd number")
+
+print()
+
+
+print()
+print("Task 2")
+print()
+x = 0
+for i in range(1,51):
+    x = x+ i
+print("The sum of number from 1 to  is:",x)
